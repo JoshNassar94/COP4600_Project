30c3ce8eb7a5909a12f3a3701fbe9f8bfa537728
Josh Nassar <joshnassar94@gmail.com>
2015-03-24 15:52:24 -0400

    Commands work without providing absolute path now

--- use_this/dev/user_created_commands.c
+++ use_this/dev/user_created_commands.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h> 
+#include <string.h>
 
 #include "user_created_commands.h"
 #include "data_structures/data_structures.h"
@@ -8,6 +10,7 @@ void execute_externel_command(linked_list * linkedlist){
 	char * command;
 	char ** arguments;
 	char ** envp = {NULL};
+	//char * envp = getenv("PATH");
 	
 
 	//count the number of elements in the linked list so we know how much memory to allocate for the array;
@@ -18,7 +21,7 @@ void execute_externel_command(linked_list * linkedlist){
 		list_element_count++;
 	}
 	
-	//allocate the memory + 1 because argument list need to end with NULL pointer
+	//allocate  the memory + 1 because argument list need to end with NULL pointer
 	arguments = malloc(sizeof(char *) * list_element_count + 1);
 	
 	int i=0;
@@ -28,12 +31,68 @@ void execute_externel_command(linked_list * linkedlist){
 		current_node = current_node->next;
 	}
 	command = arguments[0];
-	
-
-	puts(command);
-	if(execve(command, arguments, envp)<0){
-		puts("execve has failed");
+	char path[201];
+	if(command[0] != '/'){
+		if(find_path(path, command) == 0){
+			return;
+		}
+		else{
+			puts(command);
+			if(execve(path, arguments, envp)<0){
+				puts("execve has failed");
+			}
+			return;
+		}
 	}
+	else{
+		puts(command);
+		if(execve(command, arguments, envp)<0){
+			puts("execve has failed");
+		}
+	}
+}
 
+int checkifexecutable(const char* pth, const char* filename){
+	DIR *d;
+	struct dirent *dir;
+	d = opendir(pth);
+	if(d){
+		while ((dir = readdir(d))){
+			if(strcmp(dir->d_name,filename) == 0){
+				return 1;
+			}
+		}
+	}
+	return 0;
+}
 
+int find_path(char *pth, char *exe){
+	char* searchpath = getenv("PATH");
+	if(searchpath == NULL)
+		return 0;
+	if(strlen(searchpath) <= 0)
+		return 0;
+	
+	char* end;
+	char* beg = searchpath;
+	int stop = 0;
+	int found = 0;
+	int len;
+	do {
+		end = strchr(beg, ':');
+		if (end == NULL) {
+		   stop = 1;
+		   strncpy(pth, beg, 200);
+		   len = strlen(pth);
+		} else {
+		   strncpy(pth, beg, end - beg);
+		   pth[end - beg] = '\0';
+		   len = end - beg;
+		}
+		found = checkifexecutable(pth, exe);
+		if (pth[len - 1] != '/') strncat(pth, "/", 1);
+		strncat(pth, exe, 200 - len);
+		if (!stop) beg = end + 1;
+	} while (!stop && !found);
+	return found;
 }
